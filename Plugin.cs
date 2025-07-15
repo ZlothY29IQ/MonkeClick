7e369eac30cb7764f90bc08195b3b5e9e71cf100
Zloth <badzacgaming@gamil.com>
2025-07-15 17:33:39 +0100

    Line angle

--- Plugin.cs
+++ Plugin.cs
@@ -5,8 +5,7 @@ using BepInEx;
 using BepInEx.Bootstrap;
 using GorillaLocomotion;
 using UnityEngine;
-//using BananaOS.Pages;
-//using BananaOS;
+
 using GorillaInfoWatch.Attributes;
 using GorillaInfoWatch.Models;
 using GorillaInfoWatch.Models.Widgets;
@@ -16,7 +15,6 @@ using Photon.Pun;
 namespace MonkeClick
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    //[BepInDependency("Husky.BananaOS", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("dev.gorillainfowatch", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
@@ -123,11 +121,9 @@ namespace MonkeClick
 
         void Start()
         {
-            // Verify BananaOS or GorillaInfoWatch presence
-            bool hasBanana = Chainloader.PluginInfos.ContainsKey("Husky.BananaOS");
             bool hasInfo = Chainloader.PluginInfos.ContainsKey("dev.gorillainfowatch");
 
-            if (/*!hasBanana &&*/ !hasInfo)
+            if (!hasInfo)
             {
                 Logger.LogError($"MonkeClick requires GorillaInfoWatch.");
                 this.enabled = false;
@@ -170,12 +166,18 @@ namespace MonkeClick
             {
                 lineRenderer.enabled = true;
 
+                float angleOffset = 30f;
+
+                Vector3 rotatedDirection = Quaternion.AngleAxis(angleOffset, handTransform.right) * handTransform.forward;
+
                 var hits = Physics.RaycastAll(
                     handTransform.position,
-                    handTransform.forward,
+                    rotatedDirection,
                     Mathf.Infinity,
                     ~(1 << LayerMask.NameToLayer("Gorilla Boundary"))
                 );
+
+
                 Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
 
                 foreach (var hit in hits)
@@ -206,55 +208,7 @@ namespace MonkeClick
         }
     }
 
-    // BananaOS WatchPage implementation
-    /*
-    public class Page : WatchPage
-    {
-        public override string Title => "Monke Click";
-        public override bool DisplayOnMainMenu => true;
-
-        public override void OnPostModSetup()
-        {
-            selectionHandler.maxIndex = 1;
-        }
-
-        public override string OnGetScreenContent()
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine($"<color=yellow>==</color> Monke Click <color=yellow>==</color>");
-            sb.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0,
-                $"Status: {(Plugin.active ? "Enabled" : "Disabled")}"));
-            sb.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(1,
-                $"Colour: {Plugin.colors[Plugin.colorIndex].Item1}"));
-            return sb.ToString();
-        }
-
-        public override void OnButtonPressed(WatchButtonType buttonType)
-        {
-            switch (buttonType)
-            {
-                case WatchButtonType.Up:
-                    selectionHandler.MoveSelectionUp();
-                    break;
-                case WatchButtonType.Down:
-                    selectionHandler.MoveSelectionDown();
-                    break;
-                case WatchButtonType.Enter:
-                    if (selectionHandler.currentIndex == 0)
-                        Plugin.active = !Plugin.active;
-                    else
-                    {
-                        Plugin.colorIndex = (Plugin.colorIndex + 1) % Plugin.colors.Count;
-                        Plugin.RefreshColor(Plugin.colors[Plugin.colorIndex].Item2);
-                    }
-                    break;
-                case WatchButtonType.Back:
-                    ReturnToMainMenu();
-                    break;
-            }
-        }
-    }
-    */
+    
 
     // GorillaInfoWatch Screen implementation
     [ShowOnHomeScreen]
